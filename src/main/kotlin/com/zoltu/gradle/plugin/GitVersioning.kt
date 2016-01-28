7453451c82595d9554e432d5cda3f7990c035db0
Micah Zoltu <micah@zoltu.net>
2016-01-27 21:37:03 -0800

    Adds support for semantic versioning.
    Also adds tests.

--- src/main/kotlin/com/zoltu/gradle/plugin/GitVersioning.kt
+++ src/main/kotlin/com/zoltu/gradle/plugin/GitVersioning.kt
@@ -12,29 +12,51 @@ class GitVersioning : Plugin<Project> {
 	override fun apply(project: Project?) {
 		if (project == null) return;
 
-		val versionInfo = getVersionInfo(project.rootDir)
+		val describeResults = getGitDescribeResults(project.rootDir)
+		val versionInfo = getVersionInfo(describeResults)
 		setProjectVersion(project, versionInfo)
 		setJarManifestVersion(project, versionInfo)
 	}
 
-	fun getVersionInfo(rootDirectory: File): VersionInfo {
-		val repository = FileRepositoryBuilder().findGitDir(rootDirectory)!!.build()!!
-		val git = Git.wrap(repository)!!
-		val describeResults = git.describe().setLong(true).call() ?: throw Exception("Your repository must have at least one tag in it with the format `v1.2`.")
-		val regex = Regex("""v([0-9]+)\.([0-9]+)\-([0-9]+)\-g(.*)""")
+	fun getVersionInfo(describeResults: String): VersionInfo {
+		return tryGetSemanticVersionInfo(describeResults) ?: getSimpleVersionInfo(describeResults)
+	}
+
+	private fun getSimpleVersionInfo(describeResults: String): VersionInfo {
+		val regex = Regex("""v([0-9]+?)\.([0-9]+?)\-([0-9]+?)\-g(.*)""")
 		val match = regex.matchEntire(describeResults) ?: throw Exception("Git describe didn't return the expected format.")
 		val major = match.groups[1]?.value ?: throw Exception("Git describe matched the expected format but the matcher didn't return group 1.")
 		val minor = match.groups[2]?.value ?: throw Exception("Git describe matched the expected format but the matcher didn't return group 2.")
-		val patch = match.groups[3]?.value ?: throw Exception("Git describe matched the expected format but the matcher didn't return group 3.")
+		val commitCount = match.groups[3]?.value ?: throw Exception("Git describe matched the expected format but the matcher didn't return group 3.")
 		val sha = match.groups[4]?.value ?: throw Exception("Git describe matched the expected format but the matcher didn't return group 4.")
-		return VersionInfo(major, minor, patch, sha)
+		return VersionInfo(major = major, minor = minor, commitCount = commitCount, sha = sha)
 	}
 
-	fun setProjectVersion(project: Project, versionInfo: VersionInfo) {
+	private fun tryGetSemanticVersionInfo(describeResults: String): VersionInfo? {
+		val regex = Regex("""v([0-9]+?)\.([0-9]+?)\.([0-9]+?)(?:\-([0-9A-Za-z\.\-]+))?\-([0-9]+?)\-g([a-zA-Z0-9]+?)""")
+		val match = regex.matchEntire(describeResults) ?: return null
+
+		val major = match.groups[1]?.value ?: return null
+		val minor = match.groups[2]?.value ?: return null
+		val patch = match.groups[3]?.value ?: return null
+		val tags = match.groups[4]?.value
+		val commitCount = match.groups[5]?.value ?: return null
+		val sha = match.groups[6]?.value ?: return null
+
+		return VersionInfo(major = major, minor = minor, patch = patch, tags = tags, commitCount = commitCount, sha = sha)
+	}
+
+	private fun getGitDescribeResults(rootDirectory: File): String {
+		val repository = FileRepositoryBuilder().findGitDir(rootDirectory)!!.build()!!
+		val git = Git.wrap(repository)!!
+		return git.describe().setLong(true).call() ?: throw Exception("Your repository must have at least one tag in it.")
+	}
+
+	private fun setProjectVersion(project: Project, versionInfo: VersionInfo) {
 		project.version = versionInfo.toString()
 	}
 
-	fun setJarManifestVersion(project: Project, versionInfo: VersionInfo) {
+	private fun setJarManifestVersion(project: Project, versionInfo: VersionInfo) {
 		val closure = object {
 			fun apply(jar: Jar) {
 				val attributes = jar.manifest?.attributes ?: throw Exception("The Jar task has no manifest.")
